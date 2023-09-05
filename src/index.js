4a223458663f72bd367ab8a5d9db2ad223279600
Kateryna-gr <128936446+Kateryna-gr@users.noreply.github.com>
2023-09-06 00:29:28 +0300

    update

--- src/index.js
+++ src/index.js
@@ -5,50 +5,46 @@ const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const submitBtn = document.querySelector('[type="submit"]');
 const loadBtn = document.querySelector('.load-more');
+loadBtn.style.display = 'none';
 
-console.log(loadBtn);
+let pageNum = 1;
 
 submitBtn.addEventListener('click', submitHandler);
+loadBtn.addEventListener('click', loadHandler);
 
-function submitHandler(evt) {
-  evt.preventDefault();
-
-  let q = form.searchQuery.value;
-  console.log(q);
-  fetchQuery(q)
-    .then(data => console.log(data.hits))
-    .then(data => (gallery.insertAdjacentHTML = createMarkup(data.hits)))
-    .catch(() =>
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      )
-    );
-}
-
-function fetchQuery(q) {
+function fetchQuery(query) {
   const BASE_URL = 'https://pixabay.com/api/';
+  const apiKey = '39252796-33dd54a02d1582f089eb20416';
+  
   const params = new URLSearchParams({
-    key: '39252796-33dd54a02d1582f089eb20416',
-    // q: "",
+    key: apiKey,
+    q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    page: pageNum,
+    per_page: 5,
   });
 
-  return fetch(`${BASE_URL}?${params}&q=${q}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  return axios
+    .get(`${BASE_URL}?${params}`)
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error(`Request failed with status ${response.status}`);
+      }      
+      return response.data;
+    })
+    .catch(error => {
+      throw error;
+    });
 }
 
 function createMarkup(arr) {
   return arr
     .map(
-      ({ webformatURL, largeImageURL, tags, likes, views }) => `
+      ({ webformatURL, tags, likes, views, comments, downloads }) => `
     <div class="photo-card">
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <img src="${webformatURL}" alt="${tags}" height="200" loading="lazy" />
     <div class="info">
       <p class="info-item">
         <b>Likes</b>${likes}
@@ -66,6 +62,49 @@ function createMarkup(arr) {
   </div>`
     )
     .join('');
+}
+
+function submitHandler(evt) {
+  evt.preventDefault();
+
+  loadBtn.style.display = 'none';
+  gallery.innerHTML = '';  
+  let q = form.searchQuery.value;
+  fetchQuery(q)
+    .then(data => {
+      if (data.hits.length === 0) {
+        throw new Error('No images found.');
+      }
+      pageNum = 1;
+      return data;
+    })
+    .then(data => {
+      gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+      loadBtn.style.display = 'block';
+    })
+    .catch(error => {
+      console.error(error);
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    });
+}
+
+function loadHandler() {
+  pageNum += 1;
+  let q = form.searchQuery.value;
+  console.log(q);
+  fetchQuery(q)
+    .then(data => {
+      if (data.hits.length === 0) {
+        throw new Error('No images found.');
+      }      
+      return data;
+    })
+    .then(data => {
+      gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+    })
+    .catch(error => console.error(error));
 }
 
 // webformatURL - посилання на маленьке зображення для списку карток.
